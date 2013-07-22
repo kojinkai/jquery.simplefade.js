b87ccbe3ab262530188fc1c9f3555329e6ecd461
Lewis Nixon <lewcfn@gmail.com>
2013-07-22 14:24:05 +0100

    adding the typical structure and polyfilling requestAnimationFrame for the interval

--- src/simplefade.js
+++ src/simplefade.js
@@ -1,38 +1,100 @@
-/*
- * SimpleFade
- * 
+/* ==========================================================
+ * bootstrap-carousel.js v2.3.2
+ * http://twitter.github.com/bootstrap/javascript.html#carousel
+ * ==========================================================
+ * Copyright 2012 Twitter, Inc.
  *
- * Copyright (c) 2013 Lewis Nixon
- * Licensed under the MIT license.
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
 
-(function ($) {
 
-  // Collection method.
-  $.fn.awesome = function () {
-    return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+;(function ( $, window, document, undefined ) {
+
+  // Create the defaults once
+  var simpleFade = 'simpleFade',
+      defaults = {
+        interval: 5000
+      };
+
+  // polyfill rAF
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+          timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+    };
+  }
+
+  function SimpleFade(element, options) {
+    this.element = element;
+
+    // jQuery has an extend method that merges the 
+    // contents of two or more objects, storing the 
+    // result in the first object. The first object 
+    // is generally empty because we don't want to alter 
+    // the default options for future instances of the Backmenu
+    this.options = $.extend( {}, defaults, options) ;
+    
+    this._defaults = defaults;
+    this._name = simpleFade;
+    
+    this.init();
+  }
+
+  SimpleFade.prototype = {
+    init: function() {
+      $(this.element).css('position', 'relative').children().css({'position': 'absolute', 'left': 0, 'right': 0});
+    },
+    cycle: function() {
+
+    }
+  };
+
+  $.fn[simpleFade] = function ( options ) {
+    return this.each(function () {
+      new SimpleFade( this, options );
     });
-  };
+  };  
 
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+ /* simpleFade DATA-API
+  * ================= */
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
+  $(document).on('click.carousel.data-api', '[data-slide], [data-slide-to]', function (e) {
+  
+    var $this = $(this), 
+    href,
 
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
+    // regex strip for ie7
+    target = $this.attr('data-target') || e.preventDefault() || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''),
+    option = $this.data();
+    // $(target).toggleClass(option.toggle);
+    $(target).dropmenu(option);
+  }); 
 
-}(jQuery));
+})( jQuery, window, document );
