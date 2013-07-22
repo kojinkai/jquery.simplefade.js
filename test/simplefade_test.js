10715f9c16890dd0c5fb147ec077b26a5252354c
Lewis Nixon <lewcfn@gmail.com>
2013-07-22 14:23:43 +0100

    configuring some basic tests

--- test/simplefade_test.js
+++ test/simplefade_test.js
@@ -20,7 +20,7 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  module('simpleFade basics', {
     // This will run before each test in this module.
     setup: function() {
       this.elems = $('#qunit-fixture').children();
@@ -30,33 +30,32 @@
   test('is chainable', function() {
     expect(1);
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+    strictEqual(this.elems.simpleFade(), this.elems, 'should be chainable');
   });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
+  module('simpleFade state', {
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
+  test('is correct state', function() {
+    expect(2);
+    strictEqual(this.elems.simpleFade().children().css('position'), 'absolute', 'should be absolute');
+    strictEqual(this.elems.simpleFade().children().first().css('opacity'), '1', 'should be 1');
   });
+
+  // module(':awesome selector', {
+  //   // This will run before each test in this module.
+  //   setup: function() {
+  //     this.elems = $('#qunit-fixture').children();
+  //   }
+  // });
+
+  // test('is awesome', function() {
+  //   expect(1);
+  //   Use deepEqual & .get() when comparing jQuery objects.
+  //   deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
+  // });
 
 }(jQuery));
